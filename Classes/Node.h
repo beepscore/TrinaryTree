5d428d9fd9532e0c8213950b7ade0530f5298abb
beepscore <support@beepscore.com>
2010-12-05 11:35:06 -0800

    Add Node, subclass of UIView.

---
+++ Classes/Node.h
@@ -0,0 +1,16 @@
+//
+//  Node.h
+//  TrinaryTree
+//
+//  Created by Steve Baker on 12/5/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Node : UIView {
+
+}
+
+@end
---
+++ Classes/Node.m
@@ -0,0 +1,37 @@
+//
+//  Node.m
+//  TrinaryTree
+//
+//  Created by Steve Baker on 12/5/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "Node.h"
+
+
+@implementation Node
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
