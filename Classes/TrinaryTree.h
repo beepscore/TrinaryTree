d08c725e4e80b71c485ca0419b3f51e064bc0bc7
beepscore <support@beepscore.com>
2010-12-05 20:37:49 -0800

    Add Model class TrinaryTree.  It will have a collection of nodes linked in a tree structure.  It will have methods to add a node and to delete a node.

---
+++ Classes/TrinaryTree.h
@@ -0,0 +1,16 @@
+//
+//  TrinaryTree.h
+//  TrinaryTree
+//
+//  Created by Steve Baker on 12/5/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TrinaryTree : NSObject {
+
+}
+
+@end
---
+++ Classes/TrinaryTree.m
@@ -0,0 +1,14 @@
+//
+//  TrinaryTree.m
+//  TrinaryTree
+//
+//  Created by Steve Baker on 12/5/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "TrinaryTree.h"
+
+
+@implementation TrinaryTree
+
+@end
